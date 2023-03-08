46052c63472e4336769eff9ce9338cfab690544f
Brain Philemon <philbrainy@gmail.com>
2023-03-08 18:42:21 +0100

    feat: add custom response handler

---
+++ src/common/Response/Response.ts
@@ -0,0 +1,71 @@
+import { BadRequest, NotFound, Ok } from './response-type';
+export class StereoResponse {
+  static Ok<Type>(
+    data: Type,
+    message = '',
+    status?: string | number,
+  ): Ok<Type> {
+    return {
+      status,
+      data,
+      success: true,
+      message,
+    } as Ok<Type>;
+  }
+
+  static OkFailure<Type>(
+    data: Type,
+    message = '',
+    status?: string | number,
+    errorCode?: string | number,
+  ): Ok<Type> {
+    return {
+      status,
+      data,
+      success: false,
+      message,
+      errorCode,
+    } as Ok<Type>;
+  }
+
+  static BadRequest(
+    error: unknown,
+    message = '',
+    errorCode = '',
+    status = 'failed',
+  ): BadRequest {
+    return {
+      status,
+      error,
+      success: false,
+      message,
+      errorCode,
+    } as BadRequest;
+  }
+
+  static NotFoundRequest(
+    error: unknown,
+    message = '',
+    errorCode = '',
+    status = 'failed',
+  ): NotFound {
+    return {
+      status,
+      error,
+      success: false,
+      message,
+      errorCode,
+    } as NotFound;
+  }
+  static Paginated<T>(array: T, message = '', status?: string | number): Ok<T> {
+    const { links, data, meta }: any = array;
+    return {
+      status,
+      success: true,
+      data,
+      meta,
+      links,
+      message,
+    } as Ok<T>;
+  }
+}

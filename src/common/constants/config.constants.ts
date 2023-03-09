dd9122bc6228b60fd7ce95f9ba0aaf0e89031b67
Brain Philemon <philbrainy@gmail.com>
2023-03-09 14:37:33 +0100

    refactor: config constants

---
+++ src/common/constants/config.constants.ts
@@ -0,0 +1,12 @@
+export const configConstant = {
+  database: {
+    host: 'DATABASE_HOST',
+    port: 'DATABASE_PORT',
+    username: 'DATABASE_USERNAME',
+    password: 'DATABASE_PASSWORD',
+    name: 'DATABASE_NAME',
+  },
+  pagination: {
+    baseUrl: 'PAGINATION_BASE_URL',
+  },
+};
